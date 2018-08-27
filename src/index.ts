6c96334d70b2d1a7f609e2fe8923dbb19d0368b4
Seb <sebastian.szaniszlo@gmail.com>
2018-08-27 18:56:22 +0100

    Append /api to api routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import App from './app';
+import { ExpressApp, Router } from './app';
 import { attachControllers } from '@decorators/express';
 import BaseExerciseController from './controllers/BaseExerciseController';
 
 const PORT = 3000;
 
-attachControllers(App, [BaseExerciseController])
+attachControllers(Router, [BaseExerciseController])
 
-App.listen(PORT, () => {
+ExpressApp.listen(PORT, () => {
     console.log('Server listening on port ' + PORT);
 });
